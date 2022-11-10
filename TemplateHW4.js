374509b1d5c03efb34208d096371d78ee70b5695
kumarakash101 <107978632+kumarakash101@users.noreply.github.com>
2022-11-10 14:39:40 -0800

    Update TemplateHW4.js

--- TemplateHW4.js
+++ TemplateHW4.js
@@ -76,10 +76,14 @@ app.get('/capitals', (req, res) => {
   // map the output array to create an array with country names and capitals
   // check for empty data in the output array
 
-  let countries = getList(req.url)
-
+  let countries = [ ];
+  for (let i=0; i < countryData.length; i++){
+    let country = `${countryData[i].name.common?countryData[i].name.common:'Not found'} - ${countryData[i].capital?countryData[i].capital:'Not found'}`;
+    countries.push(country);
+  }
+  countries.sort();
   res.render('page', {
-    heading: 'Countries & Capitals',
+    heading: 'Countries and Capitals',
     results: countries,
   });
 });
@@ -88,12 +92,17 @@ app.get('/populous', (req, res) => {
   // filter the output array for the countries with population of 50 million or more
   // sort the resulting array to show the results in order of population
   // map the resulting array into a new array with the country name and formatted population
-
-  let populous = getList(req.url);
-
+  let pop = [];
+  for (let i=0; i < countryData.length; i++){
+    if(countryData[i].population >= 50000000){
+    let population = `${countryData[i].name.common} - ${countryData[i].population.toLocaleString()}`;
+    pop.push(population);
+    }
+    }
+    pop.sort((a,b) => a.population > b.population); 
   res.render('page', {
     heading: 'Most Populous Countries',
-    results: populous,
+    results: pop,
   });
 });
 
@@ -101,7 +110,20 @@ app.get('/regions', (req, res) => {
   // reduce the output array in a resulting object that will feature the numbers of countries in each region
   // disregard empty data from the output array
 
-  let regions = getList(req.url);
+  let elements = [];
+  const countOccurrences = (arr) =>
+  arr.reduce((prev, curr) => ((prev[curr] = ++prev[curr] || 1), prev), {});
+  for (var i = 0; i < countryData.length; i++) {
+    elements.push(countryData[i].region);
+  }
+  
+  let result = countOccurrences(elements);
+  let regions = [];
+  for (const [key, value] of Object.entries(result)) {
+    if (key !== "") {
+      regions.push(`${key}: ${value}`);
+    }
+  }
 
   res.render('page', {
     heading: 'Regions of the World',
